79d3d16c176e95da07cadba2b7df1e1af8971ba2
Fernanda Bari <3378355+fehbari@users.noreply.github.com>
2024-07-21 20:36:55 -0300

    Setup scripts

---
+++ start_streaming.bat
@@ -0,0 +1,38 @@
+@echo off
+:: Check for administrative permissions
+NET SESSION >nul 2>&1
+IF %ERRORLEVEL% NEQ 0 (
+    :: If not run as administrator, relaunch the script with elevated privileges
+    powershell -Command "Start-Process cmd.exe -ArgumentList '/c %~s0' -Verb RunAs"
+    EXIT /B
+)
+
+:: Set variables
+IF NOT DEFINED SUNSHINE_CLIENT_WIDTH set SUNSHINE_CLIENT_WIDTH=2560
+IF NOT DEFINED SUNSHINE_CLIENT_HEIGHT set SUNSHINE_CLIENT_HEIGHT=1440
+IF NOT DEFINED SUNSHINE_CLIENT_FPS set SUNSHINE_CLIENT_FPS=120
+IF NOT DEFINED SUNSHINE_CLIENT_HDR set SUNSHINE_CLIENT_HDR=true
+
+rem Set resolution using QRes
+cmd /C "C:\Tools\QRes\QRes.exe /X:%SUNSHINE_CLIENT_WIDTH% /Y:%SUNSHINE_CLIENT_HEIGHT% /R:%SUNSHINE_CLIENT_FPS%"
+
+rem Set HDR using HDRCmd
+cmd /C if "%SUNSHINE_CLIENT_HDR%"=="true" (C:\Tools\HDRTray\HDRCmd on) else (C:\Tools\HDRTray\HDRCmd off)
+
+:: rem Set FPS limit using frl-toggle
+:: cmd /C "C:\Tools\frl-toggle\frltoggle.exe %SUNSHINE_CLIENT_FPS%"
+
+rem Set FPS limit using rtss-cli
+cmd /C "C:\Tools\rtss-cli\rtss-cli.exe limit:set %SUNSHINE_CLIENT_FPS%"
+
+rem Set limiter using rtss-cli
+cmd /C "C:\Tools\rtss-cli\rtss-cli.exe limiter:set 1"
+
+rem Turn off overlay using rtss-cli
+cmd /C "C:\Tools\rtss-cli\rtss-cli.exe overlay:set 0"
+
+rem Turn off G-Sync using gsynctoggle
+C:\Tools\gsync-toggle\gsynctoggle 0
+
+:: Add a delay to ensure all commands complete before closing
+timeout /t 5 /nobreak >nul
---
+++ stop_streaming.bat
@@ -0,0 +1,36 @@
+@echo off
+:: Check for administrative permissions
+NET SESSION >nul 2>&1
+IF %ERRORLEVEL% NEQ 0 (
+    :: If not run as administrator, relaunch the script with elevated privileges
+    powershell -Command "Start-Process cmd.exe -ArgumentList '/c %~s0' -Verb RunAs"
+    EXIT /B
+)
+
+:: Set variables
+set WIDTH=3440
+set HEIGHT=1440
+set FPS=175
+set /a LIMIT=%FPS%-3
+set HDR=false
+
+rem Set resolution using QRes
+cmd /C "C:\Tools\QRes\QRes.exe /X:%WIDTH% /Y:%HEIGHT% /R:%FPS%"
+
+rem Set HDR using HDRCmd
+cmd /C if "%HDR%"=="true" (C:\Tools\HDRTray\HDRCmd on) else (C:\Tools\HDRTray\HDRCmd off)
+
+:: rem Set FPS limit using frl-toggle
+:: cmd /C "C:\Tools\frl-toggle\frltoggle.exe %LIMIT%"
+
+rem Set FPS limit using rtss-cli
+cmd /C "C:\Tools\rtss-cli\rtss-cli.exe limit:set %FPS%"
+
+rem Disable limiter using rtss-cli
+cmd /C "C:\Tools\rtss-cli\rtss-cli.exe limiter:set 0"
+
+rem Turn on G-Sync using gsynctoggle
+C:\Tools\gsync-toggle\gsynctoggle 1
+
+:: Add a delay to ensure all commands complete before closing
+timeout /t 5 /nobreak >nul
